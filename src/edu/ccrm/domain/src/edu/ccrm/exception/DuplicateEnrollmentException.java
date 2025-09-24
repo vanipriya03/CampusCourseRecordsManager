f05ec1c62b28c24b5fe03fc2da4353f762eea7ec
vanipriya03 <vani.24bey10147@vitbhopal.ac.in>
2025-09-24 23:08:21 +0530

    Delete src/edu/ccrm/domain/src/edu/ccrm/exception directory

--- src/edu/ccrm/domain/src/edu/ccrm/exception/DuplicateEnrollmentException.java
+++
@@ -1,7 +0,0 @@
-package edu.ccrm.exception;
-
-public class DuplicateEnrollmentException extends Exception {
-    public DuplicateEnrollmentException(String message) {
-        super(message);
-    }
-}
--- src/edu/ccrm/domain/src/edu/ccrm/exception/MaxCreditLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package edu.ccrm.exception;
-
-public class MaxCreditLimitExceededException extends Exception {
-    public MaxCreditLimitExceededException(String message) {
-        super(message);
-    }
-}
